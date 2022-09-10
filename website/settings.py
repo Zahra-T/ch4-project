1f003a7e92f4273c6314ef573f7e059e78ecad2c
Zahra-T <112948099+Zahra-T@users.noreply.github.com>
2022-09-10 22:57:09 +0430

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#tuxpml3+^mn9d3rq0y*3^@q9=--7z6s=g5j&p%^r5gt_a8pi3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['zahra-cv.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['zahra-cv.herokuapp.com','zahra-cv-one.herokuapp.com','127.0.0.1']
 
 
 # Application definition
